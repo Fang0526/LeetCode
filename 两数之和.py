6e2f1a59b065c215f487d57de0e9a679bf2a1760
fangl <fangl@enprecis.com>
2019-08-15 16:54:51 +0800

    更新两数之和

--- 两数之和.py
+++ 两数之和.py
@@ -9,11 +9,15 @@ class Solution(object):
         lst_result=[]
         for i in range(0,len(nums)):
             dict_result[nums[i]] = i
-        
+
         for j in range(0,len(nums)):
             complete = target - nums[j]
-            if complete in dict_result and dict_result[complete] != j and dict_result[complete] not in lst_result:
-                lst_result.append(j)
-                lst_result.append(dict_result[complete])
-        
+            if complete in dict_result and dict_result[complete] != j and [dict_result[complete],j] not in lst_result:
+                lst_result.append([j,dict_result[complete]])
+                
+ 
         return lst_result
+
+solu=Solution()
+result=solu.twoSum([3,6,4,1,2,5,9,8,7],9)
+print(result)
